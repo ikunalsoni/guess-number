3d9381cdb526c24dc532b87019ac11d11608be91
Kunal Soni <73812891+IKunalsoni@users.noreply.github.com>
2021-09-09 02:12:16 +0530

    Add files via upload

---
+++ guess game.py
@@ -0,0 +1,20 @@
+import random
+mynum = random.randint(1,100)
+choice = 0
+print('I HAVE A NUMBER IN MY MIND. CAN YOU GUESS IT?')
+while True:
+    usernum = int(input('ENTER THE GUESS'))
+    if (choice == 10):
+        print('GAME OVER')
+        break   
+    if (usernum > mynum):
+        print('YOUR NUMBER IS BIG. TRY AGAIN')
+    elif (usernum < mynum) :
+        print('YOUR NUMBER IS SMALL. TRY AGAIN')
+    else:
+        print('CONGRATULATION!!! YOU WIN')
+    choice = +1
+
+      
+
+
